677b2977d10e34ce4fde5b5dc5cdef2a348fdcba
Jangmin O <jangmin.o@gmail.com>
2024-03-07 12:19:35 +0900

    metal support  setup

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
                 "ggml-alloc.c",
                 "ggml-backend.c",
                 "ggml-quants.c",
-//                "ggml-metal.m"
+                "ggml-metal.m"
             ],
 //            resources: [.process("ggml-metal.metal")],
             publicHeadersPath: "spm-headers",
@@ -34,7 +34,7 @@ let package = Package(
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-//                .define("GGML_USE_METAL")
+                .define("GGML_USE_METAL")
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
