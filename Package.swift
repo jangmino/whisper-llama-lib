ef40e6fe4af6a51b236fe05fa3f8824fd48658d8
Jangmin O <jangmin.o@gmail.com>
2024-04-06 17:33:29 +0900

    rename libray name

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "whisper-llama-lib",
+    name: "whisperllamalib",
     platforms: [
         .iOS(.v16),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "whisper-llama-lib",
-            targets: ["whisper-llama-lib"]),
+            name: "whisperllamalib",
+            targets: ["whisperllamalib"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "whisper-llama-lib",
+            name: "whisperllamalib",
             sources: [
                 "ggml.c",
                 "llama.cpp",
